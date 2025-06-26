dfa9d21a632685f32ad6365c75c505292afee5b8
avconsultency <avconsultency09@gmail.com>
2025-06-26 16:50:16 +0530

    change folder structure

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HeaderComponent(){
     return(
         <div style={{padding:10,margin:10}}>
-            <Link href={`/emp`}>Employee</Link>
+            <Link href={`/emp/createEmp`}>Employee</Link>
         </div>
     )
 }
